2efce6c0d2f09906fe7275a7e8a9c2699091ec4d
deyan petrov <deyanp19@hotmail.com>
2023-06-01 18:24:23 +0300

    creaded pages Dashboard and Training;test content

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import Fragment from 'react';
 
 export default function MyApp(props) {
     const { Component,pageProps } = props;
   
     return (
-    <div>
-        <Head>
-          <title>Payarc</title>
-          <meta name="viewport" content="initial-scale=1, width=device-width" />
-        </Head>
-        <Component {...pageProps} />
-        </div>
+         <>
+            <Head>
+            <title>Payarc</title>
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </Head>
+            <Component {...pageProps} />
+        </>
     );
   }
   
---
+++ pages/training/index.jsx
@@ -0,0 +1,6 @@
+
+export default function Training() {
+    return (
+        <h1>Training</h1>
+    )
+}
