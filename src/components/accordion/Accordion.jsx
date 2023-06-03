8aee4577eb592be33ee31e7f528eca8cb0ebe9cb
deyan petrov <deyanp19@hotmail.com>
2023-06-03 09:12:58 +0300

    accordion finished; nice

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -2,7 +2,7 @@ import styles from './Accordion.css';
 
 
 export default function Accordion() {
- const easel3=<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-easel3-fill" viewBox="0 0 16 16">
+ const easel3=<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-easel3-fill" viewBox="0 0 16 16">
  <path d="M8.5 12v1.134a1 1 0 1 1-1 0V12h-5A1.5 1.5 0 0 1 1 10.5V3h14v7.5a1.5 1.5 0 0 1-1.5 1.5h-5Zm7-10a.5.5 0 0 0 0-1H.5a.5.5 0 0 0 0 1h15Z"/>
 </svg>
 
@@ -10,8 +10,14 @@ export default function Accordion() {
         <div className="accordion" id="accordionPayarc">
             <div className="accordion-item">
                     <h2 className="accordion-header">
-                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        {easel3} Apply app
+                        <button className="accordion-button  gy-4" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <div className="icon-easel shadow p-3 mb-5 bg-white rounded text-primary">{easel3}
+                            </div>  
+                        <div>
+
+                        <h5 className="title-accordion">Apply app</h5>
+                        <p>The Apply app is an online application tool that allows merchants to fill out and submit their onboarding application and documentation.</p>
+                        </div>
                         </button>
                     </h2>
                 <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
@@ -23,7 +29,13 @@ export default function Accordion() {
             <div className="accordion-item">
                 <h2 className="accordion-header">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        {easel3} Admin portal
+                            <div className="icon-easel shadow p-3 mb-5 bg-white rounded text-primary">{easel3}
+                            </div> 
+                        <div>
+
+                         <h5 className="title-accordion"> Admin portal</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi dolorum incidunt necessitatibus omnis veritatis dolor eum! Voluptatibus nisi iusto dignissimos modi voluptatem officiis! Fugit unde sunt labore. Ex, consequuntur ab.</p>
+                        </div>
                     </button>
                 </h2>
                 <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -35,7 +47,13 @@ export default function Accordion() {
             <div className="accordion-item">
                 <h2 className="accordion-header">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        {easel3} Merchant Portal
+                            <div className="icon-easel shadow p-3 mb-5 bg-white rounded text-primary">{easel3}
+                            </div> 
+                        <div className="title-desctiption">
+
+                        <h5 className="title-accordion">Merchant Portal</h5>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem error nihil eveniet mollitia. Cupiditate, laudantium qui dignissimos doloremque, exercitationem saepe ipsa repellendus, magni minima praesentium officia illo nihil corporis maiores!</p>
+                        </div>
                     </button>
                 </h2>
                 <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -47,7 +65,15 @@ export default function Accordion() {
             <div className="accordion-item">
                 <h2 className="accordion-header">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        {easel3} Curv POS
+                            <div className="icon-easel shadow p-3 mb-5 bg-white rounded text-primary">{easel3}
+                            </div>  
+                        <div>
+
+                        <h5 className="title-accordion">Curv POS</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis nisi sapiente eum a blanditiis placeat commodi autem et nam corporis. Illum autem quibusdam veniam ullam nesciunt qui inventore cum natus.
+                        </p>
+                        </div>
                     </button>
                 </h2>
                 <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
